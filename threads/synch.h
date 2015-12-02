0b2ed1f432b503b6e282a5baa5bd0a3ccbda234d
JJ Lau <j.j.j.lau@live.com>
2015-12-02 22:49:01 +0900

    Added in interface functions for cache.c and incorporated some into filesys.c and made a Lock typedef for synch.h

--- threads/synch.h
+++ threads/synch.h
@@ -18,12 +18,12 @@ void sema_up (struct semaphore *);
 void sema_self_test (void);
 
 /* Lock. */
-struct lock 
+typedef struct lock 
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
     struct list_elem elem;
-  };
+  } Lock;
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
